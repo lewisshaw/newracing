b303edbc21006dfebb7a708991d806b873a7c395
Lewis Shaw <lms530@york.ac.uk>
2016-09-04 21:50:22 +0100

    Removed password

--- app/Ui/src/Controller/Api/RaceImport.php
+++ app/Ui/src/Controller/Api/RaceImport.php
@@ -13,14 +13,7 @@ class RaceImport
 
     public function import(Request $request)
     {
-        $token = $this->app['security.token_storage']->getToken();
-        if (null !== $token) {
-            $user = $token->getUser();
-        }
-        $encoder = $this->app['security.encoder_factory']->getEncoder($user);
 
-// compute the encoded password for foo
-        $password = $encoder->encodePassword('436tlsegr7ogslgs7ogsglhdfsgf7ysdghsdfighf', $user->getSalt());
         return $password;
     }
 }
